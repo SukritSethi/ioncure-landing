a995854978059db1ee4c7f388056f1aadadc4a3b
Sukrit Sethi <sukritsethi18@gmail.com>
2023-12-28 03:02:32 +0530

    added react router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import React from 'react'
+// App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/Contact.jsx
@@ -0,0 +1,13 @@
+// components/Contact.js
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <div>
+      <h2>Contact Us</h2>
+      <p>Feel free to reach out to us using the contact form below.</p>
+    </div>
+  );
+};
+
+export default Contact;
